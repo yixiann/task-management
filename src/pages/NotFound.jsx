2b5a974a5f5faecfd2c4eafb8e942135fd7d522b
yixiann <yixianns@gmail.com>
2021-12-12 11:46:28 +0800

    Bypass

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { Result, Button } from "antd";
-import { Link } from "react-router-dom";
+// import { Result, Button } from "antd";
+import { Link, Navigate } from "react-router-dom";
 
 const NotFound = ({ language }) => {
   return (
-    <div style={{height:"100vh", display: "flex", alignItems: "center" }}>
-      <Result
-        status="404"
-        title="404"
-        subTitle="Sorry, the page you visited does not exist."
-        style={{margin: 'auto'}}
-        extra={
-          <Button type="primary">
-            <Link to="/overview">{language?.button?.backHome}</Link>
-          </Button>
-        }
-      />
-    </div>
+    <Navigate to ='/overview'/>
+    // <div style={{height:"100vh", display: "flex", alignItems: "center" }}>
+    //   <Result
+    //     status="404"
+    //     title="404"
+    //     subTitle="Sorry, the page you visited does not exist."
+    //     style={{margin: 'auto'}}
+    //     extra={
+    //       <Button type="primary">
+    //         <Link to="/overview">{language?.button?.backHome}</Link>
+    //       </Button>
+    //     }
+    //   />
+    // </div>
   );
 };
 
