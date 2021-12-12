9809c7489fafd0d666199e88d536549639abf9e7
yixiann <yixianns@gmail.com>
2021-12-12 09:02:36 +0800

    test

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./css/common.css";
 import "./css/custom.css";
-import {
-  PublicRoutes as publicRoutes,
-} from "./routers";
+import { PublicRoutes as publicRoutes } from "./routers";
 import { connect } from "react-redux";
 import MainLayout from "./layouts/MainLayout";
 import language from "../src/constants/Languages/english";
@@ -13,11 +11,12 @@ import language from "../src/constants/Languages/english";
 function App({ ...props }) {
   return (
     <Suspense fallback={<div>loading...</div>}>
-      <Router basename="/index.html">
+      <Router>
         <MainLayout>
           <Routes>
             {publicRoutes.map((route) => (
               <Route
+                exact
                 path={route.path}
                 element={<route.component language={language} />}
               />
