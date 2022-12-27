e1d09fff36337f304292c133927f52dfa4825922
Tan Yi Xian <>
2022-12-27 12:21:39 +0800

    Add headers

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -16,5 +16,12 @@ export async function axiosRequest(
   const baseURL = "https://stark-garden-74903.herokuapp.com/";
   axios.defaults.baseURL = baseURL;
   const result = axios[method.toLowerCase()](endpoint, payload);
+  result.headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers":
+      "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
+    "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
+  };
   return result;
 }
