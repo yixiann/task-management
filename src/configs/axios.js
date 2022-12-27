c6d667453ff2a0aede02849cf6d0061b8927fd75
Tan Yi Xian <>
2022-12-27 14:35:35 +0800

    Remove headers

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -12,16 +12,8 @@ export async function axiosRequest(
   payload = null,
   method = RequestMethod.GET
 ) {
-  // const baseURL = "localhost:8080";
   const baseURL = "https://stark-garden-74903.herokuapp.com/";
   axios.defaults.baseURL = baseURL;
   const result = axios[method.toLowerCase()](endpoint, payload);
-  result.headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Credentials": "true",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
-    "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT, DELETE",
-  };
   return result;
 }
