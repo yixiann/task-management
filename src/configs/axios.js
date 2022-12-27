0fd493644b55817b80ca89ebbe79d742663b8154
Tan Yi Xian <>
2022-12-27 15:24:59 +0800

    Allow origin

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -15,7 +15,7 @@ export async function axiosRequest(
   const baseURL = "https://stark-garden-74903.herokuapp.com/";
   // const baseURL = "http://localhost:8080"; This works
   const headers = {
-    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Origin": baseURL,
     "Access-Control-Allow-Credentials": "true",
     "Access-Control-Allow-Headers":
       "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With",
