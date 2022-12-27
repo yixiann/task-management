818d62447180b0c3b54603932d4e1b2a5230719a
Tan Yi Xian <>
2022-12-27 14:12:20 +0800

    Sort tags data

--- src/components/OverviewPage/TagsManagement/CreateTag.jsx
+++ src/components/OverviewPage/TagsManagement/CreateTag.jsx
@@ -11,38 +11,36 @@ const CreateTag = ({
   setTagColour,
 }) => {
   return (
-    <>
-      <Row justify="space-between">
-        <Col span={11}>
-          <Input
-            placeholder={language?.text.tagName}
-            style={{ width: "200px" }}
-            maxLength={18}
-            value={tagName}
-            onChange={(e) => setTagName(e.target.value)}
-          />
-        </Col>
-        <Col span={6} align="center">
-          <CustomMenu
-            colour={true}
-            language={language?.colour}
-            fields={colours}
-            value={tagColour}
-            onSelect={(e) => setTagColour(e)}
-          />
-        </Col>
-        <Col span={6} align="center">
-          <Button
-            type="primary"
-            style={{ width: "100px", marginLeft: "14px" }}
-            onClick={createTag}
-            disabled={tagName.length === 0}
-          >
-            {language?.button.create}
-          </Button>
-        </Col>
-      </Row>
-    </>
+    <Row justify="space-between">
+      <Col span={11}>
+        <Input
+          placeholder={language?.text.tagName}
+          style={{ width: "200px" }}
+          maxLength={18}
+          value={tagName}
+          onChange={(e) => setTagName(e.target.value)}
+        />
+      </Col>
+      <Col span={6} align="center">
+        <CustomMenu
+          colour={true}
+          language={language?.colour}
+          fields={colours}
+          value={tagColour}
+          onSelect={(e) => setTagColour(e)}
+        />
+      </Col>
+      <Col span={6} align="center">
+        <Button
+          type="primary"
+          style={{ width: "100px", marginLeft: "14px" }}
+          onClick={createTag}
+          disabled={tagName.length === 0}
+        >
+          {language?.button.create}
+        </Button>
+      </Col>
+    </Row>
   );
 };
 
